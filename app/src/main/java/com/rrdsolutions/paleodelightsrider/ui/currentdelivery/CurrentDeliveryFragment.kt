8337b2c83166f13e4036894b248f204b50c08561
Rinor <rinorramlifreelance@gmail.com>
2020-05-11 14:21:01 +0800

    showing phone location for CurrentDeliveryFragment.kt, now implementing destination code

--- app/src/main/java/com/rrdsolutions/paleodelightsrider/ui/currentdelivery/CurrentDeliveryFragment.kt
+++ app/src/main/java/com/rrdsolutions/paleodelightsrider/ui/currentdelivery/CurrentDeliveryFragment.kt
@@ -1,6 +1,9 @@
 package com.rrdsolutions.paleodelightsrider.ui.currentdelivery
 
+import android.Manifest
 import android.annotation.SuppressLint
+import android.app.Activity
+import android.content.pm.PackageManager
 import android.location.Location
 import android.os.Bundle
 import android.util.Log
@@ -10,11 +13,17 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.widget.Toolbar
 import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.app.ActivityCompat
 import androidx.lifecycle.ViewModelProvider
 import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.Marker
+import com.google.android.gms.maps.model.MarkerOptions
 import com.rrdsolutions.paleodelightsrider.R
 
 import kotlinx.android.synthetic.main.fragment_currentdelivery.*
@@ -73,12 +82,9 @@ class CurrentDeliveryFragment : Fragment(), OnMapReadyCallback, GoogleMap.OnMark
         layout.removeAllViews()
 //
         val currentdeliverylayout = layoutInflater.inflate(R.layout.fragment_currentdelivery_layout, null)
-        layout.addView(currentdeliverylayout)
-
-
-//        val mapFragment = childFragmentManager.findFragmentById(R.id.cmap) as SupportMapFragment
-//        mapFragment.getMapAsync(this)
-//        fusedLocationClient = LocationServices.getFusedLocationProviderClient(requireActivity().baseContext)
+        val mapFragment =childFragmentManager.findFragmentById(R.id.cmap) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(requireActivity().baseContext)
 //
 //        currentdeliverylayout.dotbutton.setOnClickListener{
 ////            val popupMenu: PopupMenu = PopupMenu(requireContext(),dotbutton)
@@ -105,7 +111,9 @@ class CurrentDeliveryFragment : Fragment(), OnMapReadyCallback, GoogleMap.OnMark
 ////                true
 ////            })
 //        }
-//        activity?.findViewById<ConstraintLayout>(R.id.loadingscreenmain)?.visibility = View.GONE
+
+        layout.addView(currentdeliverylayout)
+        activity?.findViewById<ConstraintLayout>(R.id.loadingscreenmain)?.visibility = View.GONE
     }
 
     @SuppressLint("SetTextI18n")
@@ -118,39 +126,39 @@ class CurrentDeliveryFragment : Fragment(), OnMapReadyCallback, GoogleMap.OnMark
     }
 
     override fun onMapReady(p0: GoogleMap?) {
-//        map = p0!!
-//        //declare this Fragment as target when user clicks marker
-//        map.uiSettings.isZoomControlsEnabled = true
-//        map.setOnMarkerClickListener(this)
-//
-//        if (ActivityCompat.checkSelfPermission(this.context as Activity, Manifest.permission.ACCESS_FINE_LOCATION)
-//            != PackageManager.PERMISSION_GRANTED
-//            && ActivityCompat.checkSelfPermission(this.context as Activity, Manifest.permission.ACCESS_COARSE_LOCATION)
-//            != PackageManager.PERMISSION_GRANTED) {
-//
-//            Log.d("maptest", "User permission not granted")
-//
-//            return
-//        }
-//        //**actual code starts here*//
-//        else {
-//            Log.d("maptest", "User permission granted")
-//            map.isMyLocationEnabled = true
-//            fusedLocationClient.lastLocation.addOnSuccessListener {
-//                if (it != null){
-//                    lastLocation = it
-//                    ////THIS ONE!!!!
-//                    val userLoc = LatLng(it.latitude, it.longitude)
-//                    Log.d("maptest", "lat = " + it.latitude + " lng + " + it.longitude)
-//                    val titleStr = "Your location"
-//                    map.addMarker(MarkerOptions().position(userLoc).title(titleStr)).showInfoWindow()
-//                    map.moveCamera(CameraUpdateFactory.newLatLngZoom(userLoc, 18f))
-//                    //17 is almost perfect.
-//                    Log.d("maptest", "titleStr is " + titleStr)
-//
-//                }
-//            }
-//        }
+        map = p0!!
+        //declare this Fragment as target when user clicks marker
+        map.uiSettings.isZoomControlsEnabled = true
+        map.setOnMarkerClickListener(this)
+
+        if (ActivityCompat.checkSelfPermission(this.context as Activity, Manifest.permission.ACCESS_FINE_LOCATION)
+            != PackageManager.PERMISSION_GRANTED
+            && ActivityCompat.checkSelfPermission(this.context as Activity, Manifest.permission.ACCESS_COARSE_LOCATION)
+            != PackageManager.PERMISSION_GRANTED) {
+
+            Log.d("maptest", "User permission not granted")
+
+            return
+        }
+        //**actual code starts here*//
+        else {
+            Log.d("maptest", "User permission granted")
+            map.isMyLocationEnabled = true
+            fusedLocationClient.lastLocation.addOnSuccessListener {
+                if (it != null){
+                    lastLocation = it
+                    ////THIS ONE!!!!
+                    val userLoc = LatLng(it.latitude, it.longitude)
+                    Log.d("maptest", "lat = " + it.latitude + " lng + " + it.longitude)
+                    val titleStr = "Your location"
+                    map.addMarker(MarkerOptions().position(userLoc).title(titleStr)).showInfoWindow()
+                    map.moveCamera(CameraUpdateFactory.newLatLngZoom(userLoc, 18f))
+                    //17 is almost perfect.
+                    Log.d("maptest", "titleStr is " + titleStr)
+
+                }
+            }
+        }
     }
 
     override fun onMarkerClick(p0: Marker?): Boolean {
